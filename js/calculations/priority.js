52dbd02cf5306843df65c8300797fbefedd32b40
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-09 16:08:36 +0530

    Update priority.js

---
+++ js/calculations/priority.js
@@ -0,0 +1,22 @@
+/* =========================================================
+   PRIORITY
+   Only styles with sale > 300 units
+   ========================================================= */
+
+export function calculatePriority() {
+  const styleSales = {};
+
+  AppState.filteredData.sales.forEach(r => {
+    styleSales[r.style] =
+      (styleSales[r.style] || 0) + r.units;
+  });
+
+  Object.keys(styleSales).forEach(style => {
+    if (styleSales[style] > 300) {
+      AppState.calculations.priorityByStyle[style] = "HIGH";
+    }
+  });
+
+  console.log("🔥 Priority Styles",
+    AppState.calculations.priorityByStyle);
+}
